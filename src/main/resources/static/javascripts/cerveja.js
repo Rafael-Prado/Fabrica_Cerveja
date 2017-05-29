e45ed6336b111bf3ebf55179becd1326eee65942
Rafae Prado <rafaeldacruzdoprado@hotmail.com>
2017-05-29 13:22:02 -0300

    Configurando mascara de telefone e cpf e cnpj

--- src/main/resources/static/javascripts/cerveja.js
+++ src/main/resources/static/javascripts/cerveja.js
@@ -20,8 +20,31 @@ Cerveja.MaskMoney =(function() {
 	
 })();
 
+Cerveja.MaskPhoneNumber = (function(){
+	function MaskPhoneNumber(){
+	  this.inputPhoneNumber= $('.js-phone-number');
+	}
+	MaskPhoneNumber.prototype.enable= function(){
+	   var maskBehavior = function (val) {
+		 return val.replace(/\D/g, '').length === 11 ? '(00) 00000-0000' : '(00) 0000-00009';
+		};
+		
+		var options = {
+			onKeyPress: function(val, e, field, options) {
+		     field.mask(maskBehavior.apply({}, arguments), options);
+		  }
+	   };
+		this.inputPhoneNumber.mask(maskBehavior, options);
+	}
+	
+	return MaskPhoneNumber;
+}());
+
 $(function() {
 	var maskMoney = new Cerveja.MaskMoney();
 	maskMoney.enable();
+	
+	var maskPhoneNamber = new Cerveja.MaskPhoneNumber();
+	maskPhoneNamber.enable();
 })
 
---
+++ src/main/resources/static/javascripts/cliente-mascara-cpf-cnpj.js
@@ -0,0 +1,29 @@
+var Cerveja  = Cerveja ||{};
+
+Cerveja.MascaraCpfCnpj = (function(){
+	function MascaraCpfCnpj(){
+		this.radioTipoPessoa = $('.js-radio-tipo-pessoa');
+		this.labelCpfCnpj = $('[for=cpfcnpj]');
+		this.inputCpfCnpj = $('#cpfcnpj')
+	}
+	
+	MascaraCpfCnpj.prototype.iniciar = function(){
+		this.radioTipoPessoa.on('change', onTipoPessoaAlterado.bind(this));
+	}
+	
+	function onTipoPessoaAlterado(evento){
+		var tipoPessoaSelecionada = $(evento.currentTarget);
+		this.labelCpfCnpj.text(tipoPessoaSelecionada.data('documento'));
+		this.inputCpfCnpj.mask(tipoPessoaSelecionada.data('mascara'));
+		this.inputCpfCnpj.val('');
+		this.inputCpfCnpj.removeAttr('disabled');
+	}
+	
+	
+	return MascaraCpfCnpj;
+}());
+
+$(function(){
+	var mascaraCpfCnpj = new Cerveja.MascaraCpfCnpj();
+	mascaraCpfCnpj.iniciar();
+});
